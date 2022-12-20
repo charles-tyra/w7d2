170d6efd887166d3bd450177b8f68d420c3241bd
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2022-12-20 14:37:34 -0500

    finished user model thing

--- Music_App/app/models/user.rb
+++ Music_App/app/models/user.rb
@@ -3,12 +3,15 @@ class User < ApplicationRecord
     validates :password_digest, presence: true
     validates :password, length: {minimum: 6}, allow_nil: true
 
-    before_validations :ensure_session_token
+    before_validation :ensure_session_token
     attr_reader :password
     def self.find_by_credentials(email, password)
+        @user = User.find_by(email: email)
+        return @user if @user && @user.is_password?(password)
+        return nil
     end
 
-    def is_password(pass)
+    def is_password?(pass)
         # temp_digetst = BCrypt::Password.new(password_digest)
         # password_digest.is_password?(temp_digetst)
         real_pass = BCrypt::Password.new(password_digest)
@@ -16,24 +19,24 @@ class User < ApplicationRecord
     end
 
     def password=(new_pass)
-        password = new_pass
-        password_digest = BCrypt::Password.create(new_pass)
+        @password = new_pass
+        self.password_digest = BCrypt::Password.create(new_pass)
     end
 
     def ensure_session_token
-        session_token ||= generate_session_token
+        self.session_token ||= generate_session_token
     end
 
     def reset_session_token!
-        session_token = generate_session_token
+       self.session_token = generate_session_token
     end
 
 
     private
     def generate_session_token
         loop do
-            session_token = SecureRandom::base_64
-            return session_token if !session[session_token]
+            session_token = SecureRandom::urlsafe_base64
+            return session_token unless User.exists?(session_token: session_token)
         end
     end
 
